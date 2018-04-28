d89ffeb9f8afeba8346d96e265d74f0846204f4c
Trina Lu <tri613@gmail.com>
2018-04-28 23:43:05 +0800

    modify index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>This blog is still under construction...</p>
-    <button>Test</button>
+    <h1>Recent Posts</h1>
+    <ul>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <li key={node.id}>
+          <Link to={node.fields.path}>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
 export default IndexPage;
+
+export const indexQuery = graphql`
+query IndexPageQuery {
+  allMarkdownRemark(
+    limit: 5
+    sort: {fields: [frontmatter___date], order: DESC}
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY/MM/DD")
+          }
+          fields {
+            path
+          }
+        }
+      }
+    }
+  }
+`;
