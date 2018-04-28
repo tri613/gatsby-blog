016620b526889278f4c1a6170dc90dea5895090a
Trina Lu <tri613@gmail.com>
2018-04-29 00:07:47 +0800

    modfiy h1 style

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -6,7 +6,7 @@ export default function BlogTemplate({ data }) {
   const { frontmatter, html } = markdownRemark;
 
   return <article>
-    <h1 style={{ marginTop: '2rem' }}>{frontmatter.title}</h1>
+    <h1>{frontmatter.title}</h1>
     <section style={{ marginBottom: '1.5rem' }}>
       {frontmatter.date}
       <br />
