501452eba88b1bffd1314925db1438a33beb44ee
Trina Lu <tri613@gmail.com>
2018-04-28 23:44:35 +0800

    modify blog template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -5,21 +5,19 @@ export default function BlogTemplate({ data }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
 
-  return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <h2>{frontmatter.date}</h2>
-      <h3>
-        {frontmatter.tags.map(tag => 
-          <span key={tag}>
-            <Link to={`/tags/${tag}`}>#{tag}</Link>
-            {` `}
-          </span>
-        )}
-      </h3>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
-  );
+  return <article>
+    <h1 style={{ marginTop: '2rem' }}>{frontmatter.title}</h1>
+    <section style={{ marginBottom: '1.5rem' }}>
+      {frontmatter.date}
+      <br />
+      {frontmatter.tags.map(tag => 
+        <span key={tag} style={{ marginRight: '.5rem' }}>
+          <Link to={`/tags/${tag}`}>#{tag}</Link>
+        </span>
+      )}
+    </section>
+    <section dangerouslySetInnerHTML={{ __html: html }} />
+  </article>;
 }
 
 export const pageQuery = graphql`
@@ -27,7 +25,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { title: { eq: $title } }) {
       html
       frontmatter {
-        date(formatString: "MMMM/DD/YYYY")
+        date(formatString: "MMMM/DD/YYYY HH:mm:ss")
         path
         title
         tags
