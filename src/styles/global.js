b4b62cc709694871f87718b428fdb7cc5c579c0b
Trina Lu <tri613@gmail.com>
2018-06-12 15:03:54 +0800

    add scrollbar style

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,37 @@
 import { injectGlobal } from 'styled-components';
+import { darken } from 'polished';
 import theme from './theme';
 
 export default () => injectGlobal`
   html {
     background-color: ${theme.colors.primary06};
+    ::-webkit-scrollbar-button {
+      display: none;
+      height: 13px;
+      border-radius: 0px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: ${darken(0.15, theme.colors.bg)};
+      /* background-color: ${theme.colors.primary03}; */
+      &:hover {
+        background-color: ${darken(0.25, theme.colors.bg)};
+      }
+    }
+    ::-webkit-scrollbar-track {
+      background-color: ${darken(0.05, theme.colors.bg)};
+    }
+    ::-webkit-scrollbar {
+      width: 13px;
+    }
   }
 
   * {
     box-sizing: border-box;
   }
 
+  
+  
+  
   :not(pre) > code[class*='language-'],
   pre[class*='language-'] {
     border-radius: 0.1rem;
